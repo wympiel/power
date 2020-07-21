248e7b6f5e10f41d4418934d1d2f75c7a34f6013
wympiel <wymp10@gmail.com>
2020-07-21 21:55:55 +0200

    added power function with negative exponent

--- power.py
+++ power.py
@@ -1,4 +1,17 @@
 #TODO: create power function
 
-def power(base, index):
-    return
+
+def power(base, exp):
+    neg = exp < 0
+    pos_result = pos_pow(base, abs(exp))
+    return 1/pos_result if neg else pos_result
+
+def pos_pow(base, exp):
+    if exp == 0:
+        return 1
+
+    temp =  pos_pow(base, exp // 2)
+    temp *= temp
+    return temp if exp % 2 == 0 else temp * base
+
+
--- test_power.py
+++ test_power.py
@@ -1,8 +1,28 @@
 import unittest
 
-from power import power
+from power import *
 
-class PowerTest(unittest.TestCase)
-    def _test_0_index(self):
-        self.power(5,0)
-        self.assertEqual()
+class PowerTest(unittest.TestCase):
+
+    def test_power_1_1_is_1(self):
+        self.assertEqual(power(1, 1), 1)
+
+    def test_power_2_0_is_1(self):
+        self.assertEqual(power(2, 0), 1)
+
+    def test_power_0_0_is_1(self):
+        self.assertEqual(power(0, 0), 1)
+
+    def test_power_0_2_is_0(self):
+        self.assertEqual(power(0, 2), 0)
+
+    def negative_base(self):
+        self.assertEqual(power(-2, 3), -8)
+
+    def test_negative_exp(self):
+        self.assertEqual(pow(2, -5), 0.03125)
+    def test_fraction_exp(self):
+        self.assertEqual(power(4, 0.5), 2)
+
+if __name__ == '__main__':
+    unittest.main()
